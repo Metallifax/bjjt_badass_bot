a6b0d07fa3300e6a8c85ab542dcc8266fb38c778
Metallifax <mathew_forgione@msn.com>
2022-04-12 18:35:17 -0300

    initial - echo bot 'smoke test'

---
+++ bot.py
@@ -0,0 +1,66 @@
+import os
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv()
+TOKEN = os.environ.get('TOKEN')
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# command handlers
+def start(update, context):
+    """Send a message when the command /start is issued"""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log errors caused by updates."""
+    logger.warning(f'Update "{update}" caused error "{context.error}"')
+
+
+def main():
+    """Start the bot.
+
+    Creates the updater and passes it the bot's token.
+    Makes sure to set use_context=True to use the new context based callbacks
+    """
+    updater = Updater(TOKEN, use_context=True)
+
+    # get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT.
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
