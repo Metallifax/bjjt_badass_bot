2ebe383e80cca5a360d407fd880bbdca5d57e9a8
Metallifax <mathew_forgione@msn.com>
2022-04-12 19:56:48 -0300

    changed config into webhook

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+PORT = int(os.environ.get('PORT', 5000))
 TOKEN = os.environ.get('TOKEN')
 
 # Enable logging
@@ -58,7 +59,8 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+    updater.bot.setWebhook('https://bjjt-badass-bot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT.
